b13eefba4a55b1e9cb78b0111f88830dd00551db
Nidharshana5 <166585410+Nidharshana5@users.noreply.github.com>
2024-04-12 08:06:53 +0530

    Update Numbernames1.java

--- Numbernames1.java
+++ Numbernames1.java
@@ -1,24 +1,55 @@
 import java.util.Scanner;
 
 public class NumberNames {
-    public static void main(String[] args) {
+	public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Enter the number");
+        
+        System.out.print("Enter a number: ");
         String number = scanner.nextLine();
-
         
-        char[] digits = number.toCharArray();
-
-        
-        String[] names = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
-
-        
-        for (char digit : digits) {
-            int index = Character.getNumericValue(digit);
-            System.out.print(names[index] + " ");
+        // Loop through each character in the input number
+        for (int i = 0; i < number.length(); i++) {
+            char digit = number.charAt(i);
+            String name = "";
+            
+            switch (digit) {
+                case '0':
+                    name = "zero";
+                    break;
+                case '1':
+                    name = "one";
+                    break;
+                case '2':
+                    name = "two";
+                    break;
+                case '3':
+                    name = "three";
+                    break;
+                case '4':
+                    name = "four";
+                    break;
+                case '5':
+                    name = "five";
+                    break;
+                case '6':
+                    name = "six";
+                    break;
+                case '7':
+                    name = "seven";
+                    break;
+                case '8':
+                    name = "eight";
+                    break;
+                case '9':
+                    name = "nine";
+                    break;
+                default:
+                    name = "invalid";
+            }
+            
+            System.out.print(name + " ");
         }
-
+        
         scanner.close();
     }
 }
